8d98c05d04f8a685fa6299654cf26b16ece739c1
shafeeq <barram>
2021-03-19 16:08:46 +0100

    update

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -1,9 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
   const Post = sequelize.define("post", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true
-    },
     text: {
       type: Sequelize.STRING(10000),
       allowNull: false
